d1698cc4726fc4425e8c294b15655fbf79dcc786
Cahyadinur <arcanearlaze02@gmail.com>
2022-11-15 13:38:20 +0700

    file login

---
+++ login/script.js
@@ -0,0 +1,14 @@
+var a = document.getElementById("login");
+var b = document.getElementById("register");
+var c = document.getElementById("button");
+
+function register(){
+    a.style.left = "-410px";
+    b.style.left = "10px";
+    c.style.left = "110px";
+}
+function login(){
+    a.style.left = "10px";
+    b.style.left = "410px";
+    c.style.left = "0";
+}
