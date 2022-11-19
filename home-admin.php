2861f2e88ecad0a26b5429ca30ea9db9c60b82dc
Cahyadinur <arcanearlaze02@gmail.com>
2022-11-20 04:17:15 +0700

    memperbaiki codingan dsb admin

--- home-admin.php
+++ home-admin.php
@@ -6,11 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>M Project</title>
   <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
 
   <link href="https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="/folder_css/admin.css" />
+  <link rel="stylesheet" href="folder_css/admin.css" />
 </head>
 
 <body>
@@ -18,7 +17,7 @@
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark shadow fixed-top">
     <div class="container">
-      <a class="navbar-brand" aria-current="page" href="#"> <img src="/image/Logo.png" alt="Logo" width="40"
+      <a class="navbar-brand" aria-current="page" href="#"> <img src="image/Logo.png" alt="Logo" width="40"
           class="d-inline-block align-text-top me-3" />My Project </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -74,9 +73,7 @@
   <!-- Dashboard Data Mahasiswa -->
 
   <!-- Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-    crossorigin="anonymous"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
