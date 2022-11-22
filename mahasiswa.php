e62255cad9e0be14062a335575cd1e42603f3469
ekanurseva <ekanursevas@gmail.com>
2022-11-22 21:46:06 +0700

    update mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M-PROJECT | Mahasiswa</title>
-    <link rel="stylesheet" href="folder_css/style-mhs.css">
+    <link rel="stylesheet" href="folder_css/mhs.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
