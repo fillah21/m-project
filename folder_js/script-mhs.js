f9b8246042d2c70edffa8f9a0e2aa5cdf26649b8
ekanurseva <ekanursevas@gmail.com>
2022-12-30 00:33:13 +0700

    update js mahasiswa

--- folder_js/script-mhs.js
+++ folder_js/script-mhs.js
@@ -9,3 +9,9 @@ const sidebar = document.getElementsByClassName('sidebar')[0];
 menu.addEventListener("click", function(){
     sidebar.classList.toggle('hide');
 });
+
+$(document).ready(function(){
+    $(".dd").click(function(){
+        $("#dropdown").slideToggle("slow");
+    });
+});
